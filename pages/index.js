e09f5e52306b4516eb1ebb1299b7a354853b9b27
Junhao Zhang <junhaodev@gmail.com>
2022-01-03 16:46:39 -0600

    Added some more info!

--- pages/index.js
+++ pages/index.js
@@ -11,11 +11,37 @@ export default function Home({ blogs }) {
         <title>Coder100's Blog</title>
       </Head>
 
-      {blogs.map(({ url, title, bio, date }, i) => (
-        <div key={i}>
-          <Link href={`posts/${url}`}>Click to view</Link> {title} {bio} {new Date(date).toDateString()}
-        </div>
-      ))}
+      <div className={styles.title}>Coder100&apos;s Blog</div>
+      <div className={styles.desc}>Welcome to my blog! Click around and read my blog posts!</div>
+
+      <main>
+        <article>
+          {blogs.map(({ url, title, bio, date }, i) => (
+            <div key={i}>
+              <Link href={`posts/${url}`}>
+                <a>Click to view</a>
+              </Link>
+              {title}
+              {bio}
+              {new Date(date).toDateString()}
+            </div>
+          ))}
+        </article>
+        <aside>
+          <div>
+            <a href="https://github.com/cursorweb">My Github!</a>
+          </div>
+          <div>
+            <a href="https://replit.com/@Coder100">My Replit!</a>
+          </div>
+          <div>
+            <a href="https://dev.to/cursorweb">My Dev.to!</a>
+          </div>
+          <div>
+            <a href="https://www.youtube.com/channel/UCT6iqa1_JyKmoinvyUl7nSQ?sub_confirmation=1">My Youtube Channel!</a>
+          </div>
+        </aside>
+      </main>
     </div>
   )
 }
