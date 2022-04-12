be858fae3a2472442b3e36120418c43602205a56
Junhao Zhang <junhaodev@gmail.com>
2022-04-12 17:32:11 -0500

    w   i   d   e
    sorry no more prank commits after this

--- components/nav.js
+++ components/nav.js
@@ -1,11 +1,14 @@
 import Link from "next/link";
+import Image from "next/image";
+
 import styles from "./nav.module.css";
+
 
 export function Nav() {
   return (
     <nav className={styles.nav}>
       <Link href="/">
-        <a className={styles.link}>Coder100</a>
+        <a className={styles.link}><Image alt="Logo" src="/cookie.png" layout="fill" /></a>
       </Link>
     </nav>
   );
