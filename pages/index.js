9dc8521ea8c159da0df14913bb495ceb4c90edac
Junhao Jerry Zhang <jerry_mountain@hotmail.com>
2022-12-22 10:09:28 -0600

    reorganize layout

--- pages/index.js
+++ pages/index.js
@@ -10,14 +10,26 @@ export default function Index() {
       <Meta title="Coder100" />
       <Nav />
 
-      <main>
+      <section id="home">
         <h1>Coder100</h1>
-      </main>
+        <p>[adjective] Coder</p>
+      </section>
 
-      <Link href="/blog">To Blog</Link>
+      <section id="about">
+        <h2>About</h2>
+        <p>Hi! I do coding stuff and other things too.</p>
+      </section>
 
-      <h2>Projects</h2>
-      <p>For now, check out <Link href="https://coder100s-project-list.coder100.repl.co/" target="_blank">this website</Link>, until I can get my act together.</p>
+      <section id="projects">
+        <h2>Projects</h2>
+        <p>For now, check out <Link href="https://coder100s-project-list.coder100.repl.co/" target="_blank">this website</Link>, until I can get my act together.</p>
+      </section>
+
+      <section id="blog">
+        <h2>Blog</h2>
+        <p>View my blog!</p>
+        <Link href="/blog">Click here</Link>
+      </section>
     </>
   );
 }
