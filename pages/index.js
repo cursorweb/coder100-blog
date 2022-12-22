2458e91397ef3c1ac98982649555a7d164bb278c
Junhao Jerry Zhang <jerry_mountain@hotmail.com>
2022-12-22 10:26:01 -0600

    add styling

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,16 @@ import Link from "next/link";
 import { Meta } from "../components/meta";
 import { Nav } from "../components/nav/nav";
 
+import styles from "../styles/Index.module.css";
+
+
+function Section({ children, className = "", ...props }) {
+  return (
+    <section {...props} className={styles.section + " " + className}>
+      {children}
+    </section>
+  );
+}
 
 export default function Index() {
   return (
@@ -10,26 +20,26 @@ export default function Index() {
       <Meta title="Coder100" />
       <Nav />
 
-      <section id="home">
+      <Section id="home">
         <h1>Coder100</h1>
         <p>[adjective] Coder</p>
-      </section>
+      </Section>
 
-      <section id="about">
+      <Section id="about">
         <h2>About</h2>
         <p>Hi! I do coding stuff and other things too.</p>
-      </section>
+      </Section>
 
-      <section id="projects">
+      <Section id="projects">
         <h2>Projects</h2>
         <p>For now, check out <Link href="https://coder100s-project-list.coder100.repl.co/" target="_blank">this website</Link>, until I can get my act together.</p>
-      </section>
+      </Section>
 
-      <section id="blog">
+      <Section id="blog">
         <h2>Blog</h2>
         <p>View my blog!</p>
         <Link href="/blog">Click here</Link>
-      </section>
+      </Section>
     </>
   );
 }
